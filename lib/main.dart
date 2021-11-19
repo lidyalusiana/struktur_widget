fd016ff067c073b9156e9abc77d4e52c1815e0ec
unknown <lidyalusiana15@gmail.com>
2021-11-19 22:03:14 +0800

    2 Commit : Praktek Video 3

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primaryColor: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'Flutter App',
+          style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [Icon(Icons.account_circle), Icon(Icons.access_alarm)],
+        centerTitle: true,
+      ),
+    );
+  }
+}
